2634e62f804aaedcdd33acff9b56026d3cae4746
brijaldeveloper <brijalreactnative@gmail.com>
2024-08-15 11:46:17 +0530

    add focus

--- Screen/UserInfo.js
+++ Screen/UserInfo.js
@@ -1,5 +1,5 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React, { useState } from 'react';
+import {StyleSheet,View} from 'react-native';
+import React, {useState,useEffect} from 'react';
 import Header from '../Component/Header';
 import CategoryList from '../Component/CategoryList';
 import SearchInput from '../Component/SearchInput';
@@ -33,13 +33,24 @@ const data = [
   },
 ];
 
-const UserInfo = () => {
+const UserInfo = (props) => {
 
- 
   const [searchText, setSearchText] = useState('');
   const [selectedName, setSelectedName] = useState(data[0].workerRole);
- const [userList, setUserList] = useState(WorkerInfo[selectedName]);
- const [masterList, setMasterList] = useState(WorkerInfo[selectedName]);
+  const [userList, setUserList] = useState(WorkerInfo[data[0].workerRole]);
+  const [masterList, setMasterList] = useState(WorkerInfo[data[0].workerRole]);
+
+  useEffect(() => {
+    const unsubscribe = props.navigation.addListener("tabPress", async (e) => {
+      setSearchText('');
+    setSelectedName(data[0].workerRole);
+    setUserList(WorkerInfo[data[0].workerRole]);
+    setMasterList(WorkerInfo[data[0].workerRole]);
+    })
+
+// Unsubscribe to event listener when component unmount
+ return () => unsubscribe();
+  }, [ props.navigation]);
   const searchFilterFunction = text => {
     if (text) {
       const newData = masterList.filter(item => {
@@ -54,27 +65,29 @@ const UserInfo = () => {
     setSearchText(text);
   };
 
-  const updateCategoryValue=(value)=>{
-    setSearchText('')
-    setSelectedName(value.workerRole)
-    setUserList(WorkerInfo[value.workerRole])
-    setMasterList(WorkerInfo[value.workerRole])
-  }
+  const updateCategoryValue = value => {
+    setSearchText('');
+    setSelectedName(value.workerRole);
+    setUserList(WorkerInfo[value.workerRole]);
+    setMasterList(WorkerInfo[value.workerRole]);
+  };
+ 
 
   return (
     <View style={styles.container}>
       <Header />
       <View style={styles.categoryContainer}>
         <CategoryList
-        categoryData={data}
+          categoryData={data}
           updatedValue={updateCategoryValue}
           selectedName={selectedName}
         />
       </View>
       <View style={styles.searchContainer}>
-        <SearchInput 
-        value={searchText}
-        onChangeText={text => searchFilterFunction(text)} />
+        <SearchInput
+          value={searchText}
+          onChangeText={text => searchFilterFunction(text)}
+        />
       </View>
       <UserList data={userList} />
     </View>
